7a34813b50ccb03792d98168cf6cdb39da27df7b
kajal <shetekajal66@gmail.com>
2023-02-22 07:59:16 -0800

    MyJob

--- Job_1.java
+++ Job_1.java
@@ -3,7 +3,7 @@ package JenkinsGitHub;
 public class Job_1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World through Git SCM Polling");
 
 	}
 
